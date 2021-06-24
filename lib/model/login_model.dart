da4365566a09c9e223fc222e9f508a71113a8098
YfNightWind <lyh1147557099@outlook.com>
2021-06-24 17:15:47 +0800

    暂时删除一些无用的

--- lib/model/login_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:yangocanteen/global/Global.dart';
-
-Future loginModel(String user, String pass) async {
-  return await Global.getInstance()!.dio.post(
-    '/user/login',
-    data: {"username": user, "password": pass},
-  );
-}
