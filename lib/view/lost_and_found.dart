be2c4c695cf22fceb43e2e427e2e82ac6081d71e
YfNightWind <lyh1147557099@outlook.com>
2021-12-31 11:36:42 +0800

    修改文件名以及一些细节

---
+++ lib/view/lost_and_found.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class LostAndFoundPage extends StatefulWidget {
+  const LostAndFoundPage({Key? key}) : super(key: key);
+
+  @override
+  _LostAndFoundPageState createState() => _LostAndFoundPageState();
+}
+
+class _LostAndFoundPageState extends State<LostAndFoundPage> {
+  File? _imageFile;
+  final picker = ImagePicker();
+  Future chooseImage() async {
+    final getImage = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (getImage != null) {
+        _imageFile = File(getImage.path);
+      } else {
+        print('no image selected');
+      }
+    });
+  }
+
+  //iOS照片无法显示，Android可以
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('失物招领'),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.camera),
+          onPressed: () => chooseImage(),
+        ),
+        body: _imageFile != null
+            ? Center(
+                child: Image.file(_imageFile!),
+              )
+            : Center());
+  }
+}
---
+++ lib/view/show_menu.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:yangocanteen/global/Global.dart';
+import 'package:yangocanteen/view/floors/floor2.dart';
+import 'package:yangocanteen/view/floors/floor3.dart';
+import 'package:yangocanteen/view/floors/floor4.dart';
+import 'package:yangocanteen/view/floors/floor5.dart';
+import 'package:yangocanteen/view/floors/floor6.dart';
+import 'package:yangocanteen/view/floors/floor7.dart';
+
+class ShowMenuPage extends StatefulWidget {
+  @override
+  _ShowMenuPageState createState() => _ShowMenuPageState();
+}
+
+class _ShowMenuPageState extends State<ShowMenuPage>
+    with SingleTickerProviderStateMixin {
+  List<Widget> widgets = [
+    FloorTwo(),
+    FloorThree(),
+    FloorFour(),
+    FloorFive(),
+    FloorSix(),
+    FloorSeven(),
+  ];
+  List tabs = ['2楼', '3楼', '4楼', '5楼', '6楼', '7楼'];
+  // int _index = 0;
+  late TabController _controller;
+  @override
+  void initState() {
+    _controller = new TabController(
+      length: tabs.length,
+      vsync: this,
+      initialIndex: GetTabIndex.getTabIndex,
+    );
+    _controller.addListener(() {
+      setState(() {
+        GetTabIndex.getTabIndex = _controller.index;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        child: AppBar(
+          elevation: 10,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            color: Colors.black,
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          actions: [
+            Icon(
+              Icons.food_bank,
+              color: Colors.black,
+              size: 45,
+            ),
+          ],
+          bottom: TabBar(
+              controller: _controller,
+              tabs: tabs.map((e) => Tab(text: e)).toList(),
+              labelColor: Colors.black,
+              labelStyle: TextStyle(fontSize: 17),
+              indicatorColor: Colors.orange.shade500),
+        ),
+        preferredSize: Size.fromHeight(100),
+      ),
+      body: TabBarView(
+        children: widgets,
+        controller: _controller, //添加滑动效果
+      ),
+    );
+  }
+}
